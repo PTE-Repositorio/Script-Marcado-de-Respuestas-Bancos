457209f856c3d32316306756c6e7c33a3a76f22b
PTE-Repositorio <soportecampus@iudigital.edu.co>
2025-09-25 10:26:43 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -7,3 +7,12 @@ document.getElementById('runScript').addEventListener('click', () => {
     });
   });
 });  
+
+document.getElementById('runScript_2').addEventListener('click', () => {
+  chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+    chrome.scripting.executeScript({
+      target: {tabId: tabs[0].id},
+      files: ['contentScript_2.js']
+    });
+  });
+});  
